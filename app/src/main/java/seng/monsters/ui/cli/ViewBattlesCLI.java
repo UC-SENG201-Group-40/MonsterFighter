c50ab0847b7cb050e129642a26d8431534412960
retaEgoD <davidliang343@gmail.com>
2022-05-04 16:20:55 +1200

    Fix: Implemented battle rewards functionality to viewBattlesCLI

--- app/src/main/java/seng/monsters/ui/cli/ViewBattlesCLI.java
+++ app/src/main/java/seng/monsters/ui/cli/ViewBattlesCLI.java
@@ -1,18 +1,18 @@
 package seng.monsters.ui.cli;
 
-import seng.monsters.model.GameManager;
-import seng.monsters.model.Item;
-import seng.monsters.model.Trainer;
+import seng.monsters.model.*;
 
 import java.util.InputMismatchException;
 import java.util.List;
 
 public final class ViewBattlesCLI extends TestableCLI {
 
+    private final GameManager gameManager;
     private final List<Trainer> availableBattles;
     private final Trainer player;
 
     public ViewBattlesCLI(GameManager gameManager) {
+        this.gameManager = gameManager;
         availableBattles = gameManager.getAvailableBattles();
         player = gameManager.getTrainer();
     }
@@ -35,6 +35,12 @@ public final class ViewBattlesCLI extends TestableCLI {
             if ((scannerInput > 0) && (scannerInput < availableBattles.size()+1)) {
                 final var battleTrainer = availableBattles.get(scannerInput-1);
                 BattleCLI.make(player, battleTrainer);
+                if (!player.isWhitedOut()) {
+                    battleRewards(battleTrainer);
+                    displayBattleRewards(battleTrainer);
+                } else {
+                    System.out.println("You whited out!");
+                }
             } else if (scannerInput != 0) {
                 throw new IllegalArgumentException();
             }
@@ -44,6 +50,11 @@ public final class ViewBattlesCLI extends TestableCLI {
         }
     }
 
+    private void battleRewards(Trainer loser) {
+        gameManager.setGold(gameManager.getGold() + BattleManager.goldReward(loser));
+        gameManager.setScore(gameManager.getScore() + BattleManager.scoreReward(loser));
+    }
+
     private void displayBattleOptions() {
         System.out.println("\n===========================\n");
         System.out.println("The following trainers would like to battle!");
@@ -51,15 +62,20 @@ public final class ViewBattlesCLI extends TestableCLI {
             final var battleParty = availableBattles.get(i).getParty();
             System.out.printf("%d - Trainer %d:%n",
                     i+1, i+1);
-            for (int j = 0; i < battleParty.size(); i++) {
-                final var mon = battleParty.get(i);
+            for (Monster mon : battleParty) {
                 System.out.printf("%s (Level %d)%n",
                         mon.getName(), mon.getLevel());
             }
             System.out.print("\n");
         }
         System.out.println("0 - Return to Main Menu");
+    }
 
+    private void displayBattleRewards(Trainer loser) {
+        System.out.printf("The enemy payed out %d gold!%n",
+                BattleManager.goldReward(loser));
+        System.out.printf("You gained %d score!%n",
+                BattleManager.scoreReward(loser));
     }
 
     public static void make(GameManager gameManager) {
