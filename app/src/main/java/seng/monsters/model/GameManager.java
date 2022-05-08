2cd06f7deb57fe3a54c1b3341bb2e15d1c761727
Vincent <thisoneis4business@gmail.com>
2022-05-08 12:07:55 +0700

    Fix: Revert unnecessary changes

--- app/src/main/java/seng/monsters/model/GameManager.java
+++ app/src/main/java/seng/monsters/model/GameManager.java
@@ -263,8 +263,6 @@ public class GameManager {
      * @return the monster leaving the party, or null otherwise.
      */
     public Optional<Monster> partyMonstersLeave() {
-        if (trainer.getParty().size() < 2)
-            return Optional.empty();
         for (int i = 0; i < trainer.getParty().size(); i++) {
             final var mon = trainer.getParty().get(i);
             if (mon.shouldLeave()) {
